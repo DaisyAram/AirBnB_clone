b6dc58d9283fbc039601a0d5e40766a21e0e6219
DaisyAram <daisyarama03@gmail.com>
2024-02-11 06:56:24 -0800

    pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,15 +58,15 @@ class FileStorage():
             return
 
         with open(FileStorage.__file_path, 'r') as f:
-           deserialized = None
-           try:
-               deserialized = json.load(f)
-           except json.JSONDecodeError:
-               pass
+            deserialized = None
+            try:
+                deserialized = json.load(f)
+            except json.JSONDecodeError:
+                pass
 
-           if deserialized is None:
-               return
+            if deserialized is None:
+                return
 
-           FileStorage.__objects = {
-                   k: current_classes[k.split('.')[0]](**v)
-                   for k, v in deserialized.items()}
+            FileStorage.__objects = {
+                    k: current_classes[k.split('.')[0]](**v)
+                    for k, v in deserialized.items()}
